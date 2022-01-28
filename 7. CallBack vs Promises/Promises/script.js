ba16904831c4f47fd7de9b441d2786c0a9d8c2a6
Rahulkrishnan R <rahulkrish.eee@gmail.com>
2022-01-28 19:28:18 +0530

    Update script.js

--- 7. CallBack vs Promises/Promises/script.js
+++ 7. CallBack vs Promises/Promises/script.js
@@ -1,144 +1,60 @@
-
 console.log("start");
 
 console.log("mid");
 
-function userInput(emailID, password) {
-    // hits backend...
+function loginUser(email, password) {
+    // hit backend
+    const promise = new Promise (function (resolve, reject) {
+        setTimeout(function () {
+            resolve({username: "abc"})
+        }, 2000)
+    });
 
-    setTimeout(function () {
-        console.log("setTimeout working");
-        return { username: "abc"}
-    }, 5000)
+    return promise;
+}
+
+function getDetails(username) {
+    // hit backend
+    const promise = new Promise (function (resolve, reject) {
+        setTimeout(function () {
+            resolve({username, age: 25, likes: 100})
+        }, 2000)
+    });
+
+    return promise;
+}
+
+function fetchPosts(username) {
+    // hit backend
+    const promise = new Promise (function (resolve, reject) {
+        setTimeout(function () {
+            resolve(["post 1", "post 2"])
+        }, 2000)
+    });
+
+    return promise;
 }
 
 console.log("end");
 
-const userObj = userInput("abc@gmail.com", "12345"); 
-console.log(userObj);
-
-/*output:
-script.js:2 start
-script.js:4 mid
-script.js:15 end
-script.js:18 undefined --> it shows undefined because we cannot acces the return statement inside SetTimeout
-script.js:10 setTimeout working
-*/
-
-
-//==========================================================================================================================
-
-
-console.log("start");
-
-console.log("mid");
-
-function loginUser(emailID, password, callback) {
-    // hits backend...
-
-    setTimeout(function () {
-        console.log("setTimeout working");
-        callback({ username: "abc"});
-    }, 5000)
-}
-console.log("end");
-
-loginUser("abc@gmail.com", "12345", function(userObj){
-    console.log(userObj);
-})
-
-/*output:
-script.js:27 start
-script.js:29 mid
-script.js:40 end
-script.js:35 setTimeout working
-script.js:43 {username: 'abc'}
-*/
-
-
-//=============================================================================================================
-
-
-console.log("start");
-
-console.log("mid");
-
-function loginUser(emailID, password, callback) {
-    // hits backend...
-
-    setTimeout(function () {
-        console.log("setTimeout working");
-        callback({ username: "abc"});
-    }, 5000)
-}
-function getUserDetails(username, callback) {
-     // hits backend...
-    setTimeout(function () {
-        console.log("setTimeout2 working");
-        callback({username, age: 25, likes: 100}) // here username: username is written as just username since key value is of same
-    }, 2000)
-}
-console.log("end");
-
-loginUser("abc@gmail.com", "12345", function(userObj){
-    console.log(userObj);
-
-    getUserDetails(userObj.username, function(data){
-        console.log(data);
+loginUser("abc@gmail.com", "12345")
+    .then(function(userObj) {
+        console.log(userObj);
+        return getDetails(userObj.username);
     })
-})
-
-/*output:
+    .then(function(userDetails){
+        console.log(userDetails);
+        return fetchPosts(userDetails.username);
+    })
+    .then(function(posts) {
+        console.log(posts);
+    })
+        
+/* output
 start
 mid
 end
-setTimeout working
 {username: 'abc'}
-setTimeout2 working
 {username: 'abc', age: 25, likes: 100}
+(2) ['post 1', 'post 2']
 */
-
-//=============================================================================================================
-
-
-console.log("start");
-
-console.log("mid");
-
-function loginUser(emailID, password, callback) {
-    // hits backend...
-
-    setTimeout(function () {
-        console.log("setTimeout working");
-        callback({ username: "abc"});
-    }, 5000)
-}
-function getUserDetails(username, callback) {
-     // hits backend...
-    setTimeout(function () {
-        console.log("setTimeout2 working");
-        callback({username, age: 25, likes: 100}) // here username: username is written as just username since key value is of same
-    }, 2000)
-}
-
-function fetchDetails(username, callback) {
-    //// hits backend...
-    setTimeout(function () {
-        console.log("setTimeout3 working");
-        callback(['post1', 'post2']);
-    }, 2000)
-}
-
-console.log("end");
-
-loginUser("abc@gmail.com", "12345", function(userObj){
-    console.log(userObj);
-
-    getUserDetails(userObj.username, function(data){
-        console.log(data);
-
-        fetchDetails(data.username, function(posts){
-            console.log(posts);
-        })
-    })
-})
